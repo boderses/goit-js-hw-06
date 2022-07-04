b80961e10daa95f98ecfb6dccd13614165711f26
Bohdan Ses <bohdan.ses@gmail.com>
2022-07-04 21:01:11 +0200

    update task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const boxes = document.querySelector('#boxes');
+const input = document.querySelector('#controls input');
+const create = document.querySelector('[data-create]');
+const destroy = document.querySelector('[data-destroy]');
+
+function getAmount() {
+  let amount = input.value;
+  let normalSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    let size = normalSize + i * 10;
+    const element = document.createElement('div');
+    element.style.cssText =
+      `width: ${size}px;
+            height: ${size}px;
+            background-color: rgba( ${getRandomInt()} , 
+                                    ${getRandomInt()} , 
+                                    ${getRandomInt()} )`;
+    boxes.appendChild(element);
+  };
 }
+
+function getRandomInt() {
+  return Math.floor(Math.random() * 256);
+};
+
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+};
+
+create.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
